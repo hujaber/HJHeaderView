3a3065f968c5541e33179304edba3621207f1b49
Hussein Jaber <hujaber@me.com>
2018-08-08 15:17:32 +0300

    Initial Commit

---
+++ HeaderView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeaderView
+//
+//  Created by Hussein Jaber on 8/8/18.
+//  Copyright © 2018 Hussein Jaber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
